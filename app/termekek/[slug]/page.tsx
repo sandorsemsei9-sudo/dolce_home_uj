e5f485664dbe9602410127600b7bffd3f910cd86
Győr Nyílászáró 7 (Semsei Sándor) <sandorsemsei9@gmail.com>
2026-04-09 16:48:13 +0200

    iphone aprilis9

--- app/termekek/[slug]/page.tsx
+++ app/termekek/[slug]/page.tsx
@@ -163,7 +163,7 @@ export default function TermekAdatlap({ params }: { params: Promise<{ slug: stri
 
             {/* A 3D NÉZET TARTALMA */}
             <div className="flex-1 w-full relative">
-              <CanvasViewer modelUrl={product.model_url || "/models/canvas.glb"} />
+              <CanvasViewer modelUrl={product.model_url || "/models/aprilis9.glb"} />
             </div>
 
             {/* ALSÓ SEGÍTSÉG (Csak mobilon fontos igazán) */}

0a7f594e97581e068321d4b1f7f82216e94b8ffe
Győr Nyílászáró 7 (Semsei Sándor) <sandorsemsei9@gmail.com>
2026-05-11 13:52:11 +0200

    sitemap

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,4 +1,16 @@
-export default {
+/** @type {import('next-sitemap').IConfig} */
+const config = {
   siteUrl: 'https://dolce-home.hu',
   generateRobotsTxt: true,
+  // Itt adhatod meg, mit hagyjon ki a listából:
+  exclude: [
+    '/admin',
+    '/admin/*',
+    '/kosar',
+    '/penztar',
+    '/penztar/*',
+    '/icon.png'
+  ],
 }
+
+export default config

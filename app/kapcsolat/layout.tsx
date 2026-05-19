281886dc2bde930d41af53cfbb897175cda112fe
Győr Nyílászáró 7 (Semsei Sándor) <sandorsemsei9@gmail.com>
2026-05-19 07:25:04 +0200

    kapcsolat

--- app/kapcsolat/layout.tsx
+++ app/kapcsolat/layout.tsx
@@ -1,9 +1,13 @@
-// app/kapcsolat/layout.tsx
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Kapcsolat és Segítség",
-  description: "Kérdésed van a rendelésről vagy egyedi vászonképekről? Írj nekünk, hívj minket, vagy böngészd a Gyakori Kérdéseket!",
+  description:
+    "Kérdésed van a rendelésről vagy egyedi vászonképekről? Írj nekünk, hívj minket, vagy böngészd a Gyakori Kérdéseket!",
+
+  alternates: {
+    canonical: "https://www.dolce-home.hu/kapcsolat",
+  },
 };
 
 export default function KapcsolatLayout({
